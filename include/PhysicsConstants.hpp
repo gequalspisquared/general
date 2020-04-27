f22b77d8cd2edf8cc90454e28e28589f543044e3
gequalspisquared <55361906+gequalspisquared@users.noreply.github.com>
2020-04-27 19:47:50 -0400

    Create PhysicsConstants.hpp

---
+++ include/PhysicsConstants.hpp
@@ -0,0 +1,11 @@
+/*
+Includes many useful physical constants
+All constants are in base metric units
+Created by Nick Crane
+*/
+#pragma once
+
+#define univG 6.6743e-11
+#define chargeKe 8.9387551e9
+#define boltK 1.38064852e-23
+#define univR 8.3145
