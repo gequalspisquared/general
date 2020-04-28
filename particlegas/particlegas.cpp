49e5b884e48ff611523e8c8f0e70d73174024758
gequalspisquared <55361906+gequalspisquared@users.noreply.github.com>
2020-04-28 08:24:26 -0400

    Update particlegas.cpp

--- particlegas/particlegas.cpp
+++ particlegas/particlegas.cpp
@@ -8,38 +8,36 @@ Assumes neutral particles, uses Van der Waals to approximate forces
 Can be changed to include gravity
 */
 
-
+// #include <future> // async????
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
 #include "../include/PhysicsObjects3D.hpp"
 
-#define RUNANIM 0 // used to determine whether to run anim or not
+#define RUNANIM 1 // used to determine whether to run anim or not
 #define RANDOM rand()/RAND_MAX // used for initial conditions
 
 // uses Van der Waals approximation 
-void UpdateParticleAccelerations(PARTICLE** pArray, int N, float dt, double r7, double r13)
+void UpdateParticleVelocities(PARTICLE** pArray, int N, float dt, double r8, double r14)
 {
     vec3d f, u; // force vector and direction vector
-    double mag, mag2, mag7, mag13;
+    double mag2, mag8, mag14;
 
     for(int i = 0; i < N - 1; i++) 
     {
         for (int j = i + 1; j < N; j++)
         {
             u  = pArray[j]->s - pArray[i]->s;
-            mag = u.mag();
-            mag2 = mag * mag; mag7 = mag2 * mag2 * mag2 * mag; mag13 = mag7 * mag7 / mag;
-            f = u * ( 24 * ( ( r7 / mag7 ) - ( r13 / mag13 ) ) );
-            pArray[i]->a += f * ( dt / pArray[i]->m );
-            pArray[j]->a -= f * ( dt / pArray[j]->m );
+            mag2 = u.mag2();
+            mag8 = mag2 * mag2 * mag2 * mag2; mag14 = mag8 * mag8 / mag2;
+            f = u * ( -24 * ( ( 2 * r14 / mag14 ) - ( r8 / mag8 ) ) );
+            pArray[i]->v += f * ( dt / pArray[i]->m );
+            pArray[j]->v -= f * ( dt / pArray[j]->m );
         }
     }
 }
 
-
-
-/*
+/* // trying to use std::async to thread the velocity update
 void UpdateParticleAccelerations(PARTICLE** pArray, int i, int N, float dt, double r7, double r13)
 {
     vec3d f, u;
@@ -57,15 +55,11 @@ void UpdateParticleAccelerations(PARTICLE** pArray, int i, int N, float dt, doub
 }
 */
 
-
-
 // uses the leapfrog algorithm
-void UpdateParticleKinematics(PARTICLE** pArray, int N, float dt)
+void UpdateParticlePositions(PARTICLE** pArray, int N, float dt)
 {
     for (int i = 0; i < N; i++)
     {
-        pArray[i]->s += pArray[i]->v * (dt / 2);
-        pArray[i]->v += pArray[i]->a * dt;
         pArray[i]->s += pArray[i]->v * (dt / 2);
     }
 }
@@ -76,21 +70,21 @@ void FixParticlePosition(PARTICLE** pArray, int N, float dt, int L, double& mome
     double nx, ny, nz;
     for (int i = 0; i < N; i++)
     {
-        nx = pArray[i]->s.i + pArray[i]->v.i * dt;
-        ny = pArray[i]->s.j + pArray[i]->v.j * dt;
-        nz = pArray[i]->s.k + pArray[i]->v.k * dt;
+        nx = dabs(pArray[i]->s.i + pArray[i]->v.i * dt);
+        ny = dabs(pArray[i]->s.j + pArray[i]->v.j * dt);
+        nz = dabs(pArray[i]->s.k + pArray[i]->v.k * dt);
 
-        if (nx > L || nx < 0)
+        if (nx > L / 2)
         {
             pArray[i]->v.i *= -1;
             momentum += dabs(pArray[i]->p().i) * 2;
         }
-        if (ny > L || ny < 0)
+        if (ny > L / 2)
         {
             pArray[i]->v.j *= -1;
             momentum += dabs(pArray[i]->p().j) * 2;
         }
-        if (nz > L || nz < 0)
+        if (nz > L / 2)
         {
             pArray[i]->v.k *= -1;
             momentum += dabs(pArray[i]->p().k) * 2;
@@ -102,17 +96,14 @@ void CalculatePotential(PARTICLE** pArray, int N, double r6, double r12, double&
 {
     vec3d u; // direction vector
     double mag, mag2, mag6, mag12;
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N - 1; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = i + 1; j < N; j++)
         {
-            if (j != i)
-            {
-                u  = pArray[j]->s - pArray[i]->s;
-                mag = u.mag();
-                mag2 = mag * mag; mag6 = mag2 * mag2 * mag2; mag12 = mag6 * mag6;
-                potential += 24 * ( ( 13 * r12 / mag12 ) - ( 7 * r6 / mag6 ) );
-            }
+            u  = pArray[j]->s - pArray[i]->s;
+            mag = u.mag();
+            mag2 = mag * mag; mag6 = mag2 * mag2 * mag2; mag12 = mag6 * mag6;
+            potential += 4 * ( ( r12 / mag12 ) - ( r6 / mag6 ) );
         }
     }
     printf("pot = %f\n", potential);
@@ -130,34 +121,38 @@ void UpdateInfo(PARTICLE** pArray, int N, float dt, int L, int ticks, double& mo
     }
     TEMP = 2 * KE / 3;
 
-    printf(" kT = %f, KE = %f, PV = %f, U = %f, U+KE = %f\n", TEMP, KE, PV, potential, potential + KE);
+    printf("!kT = %f, KE = %f, PV = %f, U = %f, U+KE = %f\n", TEMP, KE, PV, potential, potential + KE);
 
     momentum = potential = 0;
 }
 
 int main() 
 {
-    int N = 100;
-    static int L = 10; // length of bounding box
-    float time = 0.0f;
-    static float dt = 0.0001f;
-    int tick = 0;
-    double momentum = 0;
-    static const double r1 = 0.5; // critical distance where force is 0
+    // parameters
+    int N = 1000;
+    static int L = 100; // length of bounding box
+    static float dt = 0.01f;
+    static const double r1 = 1; // critical distance where force is 0
     int ticksPerInfoUpdate = 100;
-    double potential = 0;
+    int ticksPerFrame = 10;
 
-    // initial conditions
+    // particle properties
     double mass = 1, radius = 1;
-    double initialv = 10;
+    double initialv = 1;
 
-    // derived quantities
+    // derived quantities (do not touch)
     static const double r2 = r1 * r1;
     static const double r6 = r2 * r2 * r2;
     static const double r7 = r6 * r1;
+    static const double r8 = r6 * r2;
     static const double r12 = r6 * r6;
     static const double r13 = r12 * r1;
-    static const int V = L * L * L;
+    static const double r14 = r13 * r1;
+
+    // variables used by program (do not touch)
+    double momentum = 0, potential = 0;
+    float time = 0.0f;
+    int tick = 0;
 
     PARTICLE** particles = new PARTICLE*[N] { 0 };
 
@@ -165,21 +160,23 @@ int main()
     {
         particles[i] = new PARTICLE(
             mass, radius, 
-            vec3d((double) RANDOM * L, (double) RANDOM * L, (double) RANDOM * L),
+            vec3d((double) RANDOM * L - L / 2, (double) RANDOM * L - L / 2, (double) RANDOM * L - L / 2),
             vec3d((2 * RANDOM - 1) * initialv, (2 * RANDOM - 1) * initialv, (2 * RANDOM - 1) * initialv),
             vec3d(0, 0, 0) );
     }
 
-    while (time < 0.1)
+    while (true)
     {
+        // trying to implement std::async to thread the velocity update (most expensive function)
         // for (int i = 0; i < N - 1; i++)
         // {
-        //     p_Futures.push_back(std::async(std::launch::async, UpdateParticleAccelerations, particles, i, N, dt, r7, r13));
+        //     p_Futures.push_back(std::async(std::launch::async, UpdateParticleVelocities, particles, i, N, dt, r8, r14));
         // }
-
-
-        UpdateParticleAccelerations(particles, N, dt, r7, r13);
-        UpdateParticleKinematics(particles, N, dt);
+        
+        // leapfrog algorithm
+        UpdateParticlePositions(particles, N, dt);
+        UpdateParticleVelocities(particles, N, dt, r8, r14);
+        UpdateParticlePositions(particles, N, dt);
         FixParticlePosition(particles, N, dt, L, momentum);
         time += dt; tick++;
 
@@ -187,14 +184,18 @@ int main()
         {
             CalculatePotential(particles, N, r6, r12, potential);
             UpdateInfo(particles, N, dt, L, ticksPerInfoUpdate, momentum, potential);
+            printf("!v = %f, %f, %f\n", particles[0]->v.i, particles[0]->v.j, particles[0]->v.k);
         }
 
 #if RUNANIM==1
-        for(int i = 0; i < N; i++)
-            {
-                printf("c3 %f %f %f 1\n", particles[i]->s.i, particles[i]->s.j, particles[i]->s.k);
-            }
-            printf("F\n");
+        if (tick % ticksPerFrame == 0)
+        {
+            for(int i = 0; i < N; i++)
+                {
+                    printf("c3 %f %f %f 1\n", particles[i]->s.i, particles[i]->s.j, particles[i]->s.k);
+                }
+                printf("F\n");
+        }
 #endif
     }
 
